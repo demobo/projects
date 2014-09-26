5df61ac2ac877094efd941629dd607b1e8fc8b69
bjxl <betty@jaylin.local>
2014-09-25 17:55:26 -0700

    9/25 created circleLabel

--- carui/components/TouchArea.js
+++ carui/components/TouchArea.js
@@ -107,6 +107,101 @@ define(function(require, exports, module) {
 
     });
 
+    var SecondaryCircle = UIComponent.extend({
+        constructor:function(options) {
+            this._callSuper(UIComponent, 'constructor', options);
+            this.setOrigin([0.5, 0.5]);
+            this.setOpacity(0);
+
+            this.circleLabel = new UIElement({
+                origin: [1,1],
+                size: [60, 20],
+                color: "#707070",
+                content: "",
+                style: {
+                    background: "#00d8ff"
+                },
+                opacity: 1
+            });
+
+            this._addChild(this.circleLabel).center();
+            this.reset();
+            this.hide();
+            this.values = [0,0,0,0,0,0,0,0,0];
+        },
+
+        reset: function() {
+            this.fingers = {x:{},y:{}};
+            this.x = 0;
+            this.y = 0;
+        },
+
+        hide:function() {
+            this.halt();
+            this.setOpacity(0, {duration: 200, curve: "easeOut"});
+            this.setScale(0, 0, 1, {duration: 200, curve: "easeOut"}, function () {
+                this.emit('fingerHide');
+            }.bind(this));
+        },
+
+        show:function(data) {
+            this.count = data.count;
+            this.fingers.x[data.touch] = data.clientX;
+            this.fingers.y[data.touch] = data.clientY;
+            if (data.count == _(this.fingers.x).size()) {
+                this.setOpacity(0);
+                this.setScale(0, 0, 1);
+                if (data.count == 1) {
+                    this.circleLabel.setContent("Volume");
+                } else if (data.count == 2){
+                    this.circleLabel.setContent("Mode");
+                } else {
+                    this.circleLabel.setContent("Temperature");
+                }
+
+                this.halt();
+                this.setOpacity(1);
+                this.setScale(1, 1, 1, {duration: 1000, curve: "easeOut"}, function () {
+                    this.emit('fingerShow');
+                }.bind(this));
+                this.update(data);
+            }
+        },
+
+        update: function(data) {
+            if (data.count) {
+                this.fingers.x[data.touch] = data.clientX;
+                this.fingers.y[data.touch] = data.clientY;
+                var len = _(this.fingers.x).size();
+                if (data.count==this.count) {
+                    var x = _.reduce(this.fingers.x, function(memo, num){ return memo + num; }, 0)/len;
+                    var y = _.reduce(this.fingers.y, function(memo, num){ return memo + num; }, 0)/len;
+
+                    if (this.y) this.values[data.count] = Math.floor(this.values[data.count]-y+this.y);
+
+                    this.x = x;
+                    this.y = y;
+                    this.setPosition(
+                        x,
+                        y
+                    );
+
+                    var value = Math.max(0,Math.min(100, Math.floor(this.values[data.count]/10)));
+                    this.emit('fingerChange', {
+                        delta: data.delta[1],
+                        x: x,
+                        y: y,
+                        count: this.count,
+                        color: this.color,
+                        value: value
+                    });
+                }
+            }
+        }
+    });
+
+
+
     var TouchArea = UIContainer.extend({
         constructor: function(options) {
             UIContainer.prototype.constructor.call(this, options);
@@ -121,23 +216,34 @@ define(function(require, exports, module) {
             var fingerCircle = new FingerCircle();
             fingerCircle._eventHandler.pipe(this._eventHandler);
 
-            inputSurface.on('touchStart', function(data){
+            var secondaryCircle = new SecondaryCircle();
+            secondaryCircle._eventHandler.pipe(this._eventHandler);
+
+            inputSurface.on('touchStart', function (data) {
                 fingerCircle.show(data);
+                secondaryCircle.show(data);
             });
 
-            inputSurface.on('touchUpdate', function(data){
+            inputSurface.on('touchUpdate', function (data) {
                 fingerCircle.update(data);
+                secondaryCircle.update(data);
             });
 
-            inputSurface.on('touchEnd', function(){
+            inputSurface.on('touchEnd', function () {
                 fingerCircle.reset();
                 fingerCircle.setDelay(200, fingerCircle.hide.bind(fingerCircle));
+                secondaryCircle.reset();
+                secondaryCircle.setDelay(200, secondaryCircle.hide.bind(secondaryCircle));
             });
 
+
             this._addChild(fingerCircle);
+            this._addChild(secondaryCircle);
             this._addChild(inputSurface);
         }
+
     });
+
 
     module.exports = TouchArea;
 });
