80e2b86b5bada5977a6afa06492eb9676bc523d6
yellowbang <astrotabon@gmail.com>
2014-09-28 00:34:13 -0700

    add testMeter

--- examples/testMeter.js
+++ examples/testMeter.js
@@ -6,7 +6,7 @@ define(function(require, exports, module) {
     var UIApplication       = require('containers/UIApplication');
     var UIButton            = require('controls/UIButton');
 
-    var stepHeight = 10;
+    var stepHeight = 100;
     var stepWidth = 50;
     var margin = 1;
 
@@ -20,21 +20,21 @@ define(function(require, exports, module) {
     var touchArea = new UIElement({
         size:[undefined, undefined]
     });
+    touchArea.setPosition(0, 0, 1);
 
     // Element steps are UIElements
-    var Element = UIElement.extend({
+    var MeterBar = UIElement.extend({
         constructor: function(options) {
-            options.yPosition = options.i*(stepHeight+margin);
+            options.yPosition = (options.i-1)*(stepHeight+margin);
             options.style = {
-                background: 'hsl('+options.i*30+',90%,90%)'
+                background: '#00d8ff'
             };
             UIElement.prototype.constructor.call(this, options);
             this._showing = false;
-            this.setPosition(-stepWidth+10, this.options.yPosition, 0);
+            this.setPosition(-stepWidth+1, this.options.yPosition, 0);
             this.setOpacity(0.1);
             touchArea.on('mousedown', function(event){
                 this.initPos = event.clientY;
-//                this.shift = ((event.clientY % (stepHeight + margin)) - stepHeight/2);
                 if (this.options.yPosition + stepHeight > event.clientY && this.options.yPosition < event.clientY){
                     if (this._showing === false){
                         this.toShow();
@@ -71,7 +71,7 @@ define(function(require, exports, module) {
         },
 
         toHide: function() {
-            this.setPosition(-stepWidth+10, this.options.yPosition, 0, stepTransition);
+            this.setPosition(-stepWidth+1, this.options.yPosition, 0, stepTransition);
             this.setOpacity(0.1 , stepTransition);
             this._showing = false;
         },
@@ -82,13 +82,50 @@ define(function(require, exports, module) {
 
     });
 
+    // Element steps are UIElements
+    var MeterDot = UIElement.extend({
+        constructor: function(options) {
+            options.yPosition = (options.i-1)*(stepHeight+margin);
+            options.content = ".";
+            options.style = {
+                marginTop: stepHeight + margin - 17 +'px',
+                color: '#00d8ff'
+            };
+            UIElement.prototype.constructor.call(this, options);
+            this._showing = false;
+            this.setPosition(0, this.options.yPosition, 0);
+            this.setOpacity(0);
+            touchArea.on('mousedown', function(event){
+                this.toShow();
+            }.bind(this));
+            touchArea.on('mouseup', function(event){
+                this.toHide();
+            }.bind(this));
+        },
+
+        toShow: function() {
+            this.setOpacity(1, stepTransition);
+        },
+
+        toHide: function() {
+            this.setOpacity(0, stepTransition);
+        }
+    });
+
     // The periodic table is an UIContainer
     var meterBar = new (UIContainer.extend({
         constructor: function() {
             UIContainer.prototype.constructor.call(this);
             // create Steps for each step in meter
-            for (var i = 0; i < window.innerHeight/stepHeight+1; i++) {
-                var element = new Element({
+            for (var i_bar = 0; i_bar < window.innerHeight/stepHeight+2; i_bar++) {
+                var element_bar = new MeterBar({
+                    i: i_bar,
+                    size: [stepWidth, stepHeight]
+                });
+                this._addChild(element_bar);
+            }
+            for (var i = 0; i < window.innerHeight/stepHeight+2; i++) {
+                var element = new MeterDot({
                     i: i,
                     size: [stepWidth, stepHeight]
                 });
@@ -98,6 +135,7 @@ define(function(require, exports, module) {
                 shift = (event.clientY % (stepHeight + margin)) - stepHeight/2;
                 this.setPosition(0, shift, 0)
             }.bind(this));
+
         },
 
         setSize: function(){
