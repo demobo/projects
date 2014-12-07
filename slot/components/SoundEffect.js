886a1f275ce9823fd44af272862cea9728b59d8b
Kireric <kireric@hotmail.com>
2014-12-06 19:26:23 -0800

    Sound  Effect 2.0

--- slot/components/SoundEffect.js
+++ slot/components/SoundEffect.js
@@ -1,14 +1,44 @@
 define(function(require, exports, module) {
-    var loadSoundEffect = _.memoize(function() {
+    var loadSoundEffect;
+    loadSoundEffect = _.memoize(function () {
         return {
             slot: new Howl({
-                urls: ['assets/audio/slotSound2.mp3'],
+                urls: ['assets/audio/slotSound.mp3'],
                 volume: 0.1
             }),
             paysoff: new Howl({
                 urls: ['assets/audio/paysOff.mp3'],
                 volume: 0.1
+            }),
+            backgroundmusic: new Howl({
+                urls: ['assets/audio/backGroundmusicO.mp3'],
+                volume: 0.1
+            }),
+            stopsound: new Howl({
+                urls: ['assets/audio/beep.mp3'],
+                volume: 0.1
+            }),
+            cashout: new Howl({
+                urls: ['assets/audio/cashOut.mp3'],
+                volume: 0.1
+            }),
+            insertcoin: new Howl({
+                urls: ['assets/audio/insertCoin.mp3'],
+                volume: 0.1
+            }),
+            tap: new Howl({
+                urls: ['assets/audio/tap.aiff'],
+                volume: 0.1
+            }),
+            credit: new Howl({
+                urls: ['assets/audio/smallPayoutB.mp3'],
+                volume: 0.1
+            }),
+            line: new Howl({
+                urls: ['assets/audio/line.mp3'],
+                volume: 0.1
             })
+
         };
     });
     module.exports = loadSoundEffect();
