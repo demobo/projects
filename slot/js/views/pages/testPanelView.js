5c332ad508519bba6f9dc12975909faae90c6138
kouyang119 <oy1619@hotmail.com>
2014-12-11 12:47:28 -0800

    test flipper

--- slot/js/views/pages/testPanelView.js
+++ slot/js/views/pages/testPanelView.js
@@ -3,11 +3,15 @@ define(function(require, exports, module) {
     var Engine = require('famous/core/Engine');
     var Surface = require('famous/core/Surface');
     var Modifier = require('famous/core/Modifier');
+    var Transform = require('famous/core/Transform');
+    var StateModifier = require('famous/modifiers/StateModifier');
+    var UIElement = require('core/UIElement');
+    var Flipper    = require("famous/views/Flipper");
+    var Easing = require('famous/transitions/Easing');
+
     var soundEffect = require('js/configs/SoundEffect');
     var slotGame = require('js/models/slotGame');
 
-    var Transform = require('famous/core/Transform');
-    var StateModifier = require('famous/modifiers/StateModifier');
 
     function TestPanelView(options){
 
@@ -44,21 +48,88 @@ define(function(require, exports, module) {
             //apply shadow
         });
 
-        this.surface = new Surface({
-           //define size, align in center
+        this.flipper = new Flipper();
+
+        this.frontSurface = new UIElement({
+            origin: [.5,.5],
+            align: [.5,.5],
+            size:[600,200],
+            content : 'Touch to Start',
+            style: {
+                textAlign: 'center',
+                fontSize:'60px',
+                lineHeight: '100px',
+                padding:'50px',
+                color: 'white',
+                background: 'url(assets/imgs/start-button.png)',
+                backgroundRepeat:'no-repeat'
+
+
+
+            }
         });
 
+        this.backSurface= new UIElement({
+            origin: [.5,.5],
+            align: [.5,.5],
+            size:[800,500],
+            content : 'Game Start',
+            style: {
+                textAlign: 'center',
+                lineHeight: '60px',
+                fontSize: '30px',
+                color: '#000',
+                backgroundColor:'blue'
+            }
+        });
+
+       this.centerModifier = new StateModifier({
+            align: [0.5,0.5],
+            origin:[0.5,0.5]
+        })
+
+        this.flipper.setBack(this.backSurface);
+        this.flipper.setFront(this.frontSurface);
+
+
+        this.add(this.centerModifier).add(this.flipper);
+
+
     }
+
+
 
     function _setListeners(){
         this.buttonShadow.on('click', function(){
             //make shadow change
         });
 
-        this.surface.on('click', function(){
-            //make surface turn around`
-        });
+        //this.surface.on('click', function(){
+        //    //make surface turn around`
+        //});
+
+        var toggle = false;
+        Engine.on('click', function(){
+            var angle = toggle ? 0 : Math.PI;
+            if (toggle) {
+                this.backSurface.setScale(1, 1, 1, {method: 'spring'},function(){
+                    this.flipper.setAngle(angle, {curve: Easing.inQuad, duration : 1000});
+                }.bind(this));
+            } else {
+                this.flipper.setAngle(angle, {curve: Easing.inQuad, duration : 1000}, function(){
+                    this.backSurface.setScale(1.5, 1.5, 1, {
+                        method: 'spring'
+                    });
+                }.bind(this));
+            }
+            toggle = !toggle;
+        }.bind(this));
+
+
+
     }
+
+
 
     module.exports = TestPanelView;
 
