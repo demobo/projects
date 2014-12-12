00d83c31d19186f099d27195ceeadded4fd3a5dd
kireric <kireric@hotmail.com>
2014-12-12 13:27:58 -0800

    demo ready

---
+++ slot/js/views/components/testSlotColumn.js
@@ -0,0 +1,77 @@
+define(function(require, exports, module) {
+    var UIComponent         = require('core/UIComponent');
+    var UIElement           = require('core/UIElement');
+    var UIContainer         = require('containers/UIContainer');
+    var soundEffect         = require('js/configs/SoundEffect');
+    var testSlotItem          = require('js/views/components/testSlotItem');
+
+
+    var testSlotColumn = UIContainer.extend({
+        constructor:function(options) {
+            this._callSuper(UIContainer, 'constructor', options);
+            this.options = options;
+            this.rowCount = options.rowCount;
+            var columnWidth = window.innerWidth*.66/options.dimension[0];
+            var columnHeight = this.rowCount*window.innerHeight*.7/options.dimension[1];
+            this.rowHeight = columnHeight/this.rowCount;
+            this.rows = [];
+            for (var i=0; i<this.rowCount; i++) {
+                var index = i;
+                var r = new testSlotItem({
+                    row: index,
+                    column: options.column,
+                    align: [options.column/options.dimension[0],1],
+                    origin: [0,1],
+                    position: [0, -i*this.rowHeight, 0],
+                    size: [columnWidth,this.rowHeight],
+                    style: {
+                        lineHeight: this.rowHeight+'px',
+                        fontSize: '30px',
+                        color: "#000099",
+                        textAlign: 'center'
+//                        borderTop: 'solid 1px #000'
+                    },
+                    map: options.map
+                });
+                r.update();
+                this.rows.push(r);
+                this._addChild(r);
+            }
+        },
+
+        hide: function() {
+            this.setOpacity(0, {duration: 200, curve: "easeOut"});
+        },
+
+        show: function() {
+            this.halt();
+            this.setOpacity(1, {duration: 200, curve: "easeOut"});
+        },
+
+        spin: function(duration) {
+            this.setPosition(0,this.rowHeight*(this.rowCount-this.options.dimension[1]-1),0, {
+                duration: duration,
+                curve: 'easeIn'
+            }, function() {
+                soundEffect.stopspin.play();
+                this.setPosition(0,this.rowHeight*(this.rowCount-this.options.dimension[1]),0, {
+                    method: 'snap'
+                }, function() {
+                    setTimeout(function(){
+                        this.setPosition(0,0,0);
+                        this.update();
+                    }.bind(this), 500);
+                }.bind(this));
+            }.bind(this));
+        },
+
+        update: function() {
+            _(this.rows).map(function(row){
+                row.update();
+            }.bind(this));
+        }
+
+    });
+
+    module.exports = testSlotColumn;
+});
--- slot/test.js
+++ slot/test.js
@@ -1,21 +1,124 @@
 define(function(require, exports, module) {
     var Engine              = require('famous/core/Engine');
-    var TestPanelView       = require('js/views/pages/testPanelView');
+    var StateModifier       = require('famous/modifiers/StateModifier');
+    var Flipper             = require('famous/views/Flipper');
+    var Easing              = require('famous/transitions/Easing');
 
-//    demobo_r="1234";
-//    demobo_guid = "mobile";
-//    demobo.init({
-//        isHost: false,
-//        appName: "slotMachine",
-//        alwaysOn: true,
-//        layers: ["websocket:8010", "firebase"]
-//    });
+    var UIElement           = require('core/UIElement');
+    var CoinsMainView       = require('js/views/pages/coinsMainView');
+    var testSlotMachine     = require('js/views/components/testSlotMachine');
+    var CreditBox           = require('js/views/components/CreditBox');
+    var slotGame            = require('js/models/slotGame');
+    var PushButton          = require('js/views/pages/PushButton');
 
     var mainContext = Engine.createContext();
+
+//    demobo_r="1234";
+//    demobo_guid = "web";
+//    var layers = ["websocket:8010"];
+//    demobo.init({
+//        isHost: true,
+//        appName: "slotMachine",
+//        method: "code",
+//        layers: layers
+//    });
+
+    var coinsMainView = new CoinsMainView();
+    mainContext.add(coinsMainView);
+
+    var gameStart = new PushButton({
+        origin: [.5,.5],
+        align: [.5,.5],
+        content: '<img src="assets/imgs/start-button.png"/>',
+        size: [600,190],
+        shadowSize: [575,173],
+        position: [0,-70,0],
+        boxShadowAfter: 'inset 0px 0px 100px',
+        borderRadius: '150px'
+    });
+
+    var slotMachine = new testSlotMachine({
+        size: [window.innerWidth*.7,window.innerHeight*.7],
+        dimension: [5, 3],
+        rowCount: 40
+    });
+    var slotMod = new StateModifier({
+        align: [.505,.55],
+        origin: [.5,.5]
+    });
+    var creditBox = new CreditBox({
+
+    });
+    var creditBoxMod = new StateModifier({
+        size: [200,60],
+        align: [1,1],
+        origin: [1,1]
+    });
+
+    mainContext.add(slotMod).add(slotMachine);
+    mainContext.add(creditBoxMod).add(creditBox);
+
+    var centerModifier = new StateModifier({
+        align: [0.5,0.5],
+        origin:[0.5,0.5]
+    });
+
+    var flipper = new Flipper();
+    mainContext.add(centerModifier).add(flipper);
+
+    flipper.setBack(slotMachine);
+    flipper.setFront(gameStart);
+
+    var toggle = false;
+    gameStart.buttonShadow.on('click', function(){
+        var angle = toggle ? 0 : Math.PI;
+        if (toggle) {
+            //slotMachine.setScale(1, 1, 1, {method: 'spring'},function(){
+            flipper.setAngle(angle, {curve: Easing.inQuad, duration : 1000});
+            //}.bind(this));
+        } else {
+            flipper.setAngle(angle, {curve: Easing.inQuad, duration : 1000}, function(){
+                //slotMachine.setScale(1.5, 1.5, 1, {
+                //    method: 'spring'
+                //});spring
+            }.bind(this));
+        }
+        toggle = !toggle;
+    }.bind(this));
+
+    var spin = _.debounce(function() {
+        var lines = slotGame.get('lines');
+        var credit = slotGame.get('credit')-lines;
+        if (credit>=0) {
+            slotMachine.spin();
+            slotGame.save('credit',credit);
+        }
+    },3000, true);
+
+    slotGame.on('change:button0',function(model, value){
+        console.log(model, value)
+        if (value<(Date.now()-3000)) return;
+        slotGame.save('lines',1);
+        spin.call(this);
+    }.bind(this));
+
+    slotGame.on('change:button1',function(model, value){
+        if (value<(Date.now()-3000)) return;
+        slotGame.save('lines',3);
+        spin.call(this);
+    }.bind(this));
+
+    slotGame.on('change:button2',function(model, value){
+        if (value<(Date.now()-3000)) return;
+        slotGame.save('lines',5);
+        spin.call(this);
+    }.bind(this));
+
+    slotGame.on('change:button3',function(model, value){
+        if (value<(Date.now()-3000)) return;
+        spin.call(this);
+    }.bind(this));
+
     mainContext.setPerspective(1000);
-
-    var testPanelView = new TestPanelView();
-
-    mainContext.add(testPanelView);
-
+    window.slotGame = slotGame;
 });
